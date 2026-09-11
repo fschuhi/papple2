b54b7ecc97f77879449cfe537e27d652ec8a0fa4
fschuhi <fschuhi@yahoo.com>
2026-09-11 13:05:31 +0200

    now M2.5 refactoring

--- tests/test_emulator.py
+++ tests/test_emulator.py
@@ -5,4 +5,6 @@ from papple2.Emulator import Emulator
 class TestEmulator(unittest.TestCase):
 
     def test_create_no_display(self):
+        # 2026-09-11 This results in `AttributeError: 'Display' object has no attribute 'screen'`
+        # see `TODO.md` for details on M2.5
         emulator = Emulator(no_display=True)
