fbf7ea5214e0f33ab6855efc3c1d26fdebb096f6
fschuhi <fschuhi@yahoo.com>
2026-09-24 08:58:54 +0200

    type hint sweep

--- src/papple2/util.py
+++ src/papple2/util.py
@@ -2,86 +2,59 @@
 
 import itertools
 import tomllib
+from collections.abc import Iterable, Iterator
 from pathlib import Path
 
-def chunks(l, n):
+def chunks[T](l: list[T], n: int) -> Iterator[list[T]]:
     """Yield successive n-sized chunks from l."""
     # https://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks
     for i in range(0, len(l), n):
         yield l[i:i + n]
 
-def signed(x):
+def signed(x: int) -> int:
     if x > 0x7F:
         x -= 0x100
     return x
 
 
-def group(iterable):
-    "s -> (s0,s1), (s2,s3), (s4, s5), ..."
-    # https://stackoverflow.com/questions/5389507/iterating-over-every-two-elements-in-a-list
-    a = iter(iterable)
-    return zip(a, a)
-
-
-def pairwise(iterable):
+def pairwise[T](iterable: Iterable[T]) -> Iterator[tuple[T, T]]:
     "s -> (s0,s1), (s1,s2), (s2, s3), ..."
     a, b = itertools.tee(iterable)
     next(b, None)
     return zip(a, b)
 
 
-def verbose_info(info):
-    return hexaddr(info.address)
+def hex2int(text: str) -> int:
+    text = text if text[:1] != "$" else text[1:]
+    return int(text, 16)
 
 
-def verbose_enum(enum):
-    return "none" if enum is None else str(enum).split(".", 1)[1]
-
-
-def verbose_address_set(s):
-    if s is None:
-        return ""
-    else:
-        addresses = map(hexaddr, sorted(s))
-        return " ".join(addresses)
-
-def verbose_address_list(l):
-    if l is None:
-        return ""
-    else:
-        addresses = map(hexaddr, sorted(l))
-        return " ".join(addresses)
-
-
-def hex2int(hex) -> int:
-    hex = hex if hex[:1] != "$" else hex[1:]
-    return int(hex, 16)
-
-
-def hexaddr(address, show_dollar=True, lower=True):
+def hexaddr(address: int | None, show_dollar: bool = True, lower: bool = True) -> str:
     res = "-" if address is None else ("$" if show_dollar else "") + hex(address)[2:].zfill(4)
     return res.lower() if lower else res.upper()
 
 
-def hexbyte(address, lower=True):
-    res = "-" if address is None else hex(address)[2:].zfill(2)
+def hexbyte(byte: int | None, lower: bool = True) -> str:
+    res = "-" if byte is None else hex(byte)[2:].zfill(2)
     return res if lower else res.upper()
 
-def hexbytes(bytes):
-    return list(map(lambda b: hexbyte(b), bytes))
+def hexbytes(values: Iterable[int]) -> list[str]:
+    return list(map(lambda b: hexbyte(b), values))
 
-def dot_RGB( R, G, B ):
+def dot_RGB( R: int, G: int, B: int ) -> str:
     # https://www.graphviz.org/doc/info/attrs.html#k:color
     return '"#%02x%02x%02x"' % (R, G, B)
 
-def Ascii2Apple2Ascii(char):
+def Ascii2Apple2Ascii(char: str | int) -> int:
     if isinstance(char, str): char = ord(char)
     return 0x80 + (char & 0x7F)
 
-def Apple2Ascii2Ascii(char):
+def Apple2Ascii2Ascii(char: int) -> int:
     return char & 0x7F
 
-def lerp_rgb(a, b, t):
+def lerp_rgb(
+    a: tuple[float, float, float], b: tuple[float, float, float], t: float
+) -> tuple[float, float, float]:
     return (
         a[0] + (b[0] - a[0]) * t,
         a[1] + (b[1] - a[1]) * t,
